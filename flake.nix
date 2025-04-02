8df80073664e9a6defe1ac3c6d7a8ec283593e10
ImNotTwig <TwigTheCat@disroot.org>
2025-04-02 05:47:26 -0500

    nix: init

--- flake.nix
+++ flake.nix
@@ -1,16 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    crane,
+    ...
+  }: let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+    craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+    
+    moldDevShell = craneLib.devShell.override {
+      # For example, use the mold linker
+      mkShell = pkgs.mkShell.override {
+        stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+      };
+    };
+    
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        pkgs.cargo
-      ];
+    devShells.${system}.default = moldDevShell {
+      RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+    };
+    packages = {
+      cta = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource ./.;
+      };
     };
   };
 }
