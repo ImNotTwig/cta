cd753331aa6d16decb5bf24efce9d3a8afb8d5f1
ImNotTwig <TwigTheCat@disroot.org>
2026-03-17 13:25:59 -0500

    downgraded nixpkgs instance

--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
 {
-  pkgs ?
-    import <nixpkgs> {
+  pkgs ? let
+    nixpkgs = builtins.fetchTarball {
+      name = "nixpkgs";
+      url = "https://github.com/nixos/nixpkgs/archive/9dcb002ca1690658be4a04645215baea8b95f31d.tar.gz";
+      sha256 = "sha256-9jVDGZnvCckTGdYT53d/EfznygLskyLQXYwJLKMPsZs=";
+    };
+  in
+    import nixpkgs {
       system = "x86_64-linux";
       overlays = [
         (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/2c7510a559416d07242621d036847152d970612b.tar.gz"))
